d7efe722b01ed673d76c816d7a4deaa8d090f726
Gaurav Kaushik <gk480555@gmail.com>
2026-06-14 01:51:09 +0530

    Update vite.config.js

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/query': 'http://127.0.0.1:8000',
-      '/response': 'http://127.0.0.1:8000',
-      '/health': 'http://127.0.0.1:8000',
-    },
-  },
 })
